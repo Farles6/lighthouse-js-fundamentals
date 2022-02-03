b021387caa378aefb8ab326ccb2b83d63aa0541d
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-02-03 15:14:31 -0500

    Add more examples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,4 +1,21 @@
-const temperature = -20;
+const raining = false;
+const cold = false;
+
+if (raining){
+  console.log("Don't forget your umbrella!");
+}
+if (cold){
+  console.log("Make sure to pick out a scraf!");
+}
+console.log("Now you're ready to go outside");
+ 
+if(cold){
+  console.log("Make sure to pick out a scraf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+const temperature = -50;
 
 if (temperature < 0){
   console.log("Make sure you pick out a scarf!");
@@ -8,3 +25,18 @@ if (temperature < 0){
   console.log("Short sleeves are fine.");
 }
 console.log("Now you're ready to go outside!")
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18){
+  console.log(" You are eligible to vote");
+}
+
+if (temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining){
+  console.log("Leave your umbrella at home!");
+}
