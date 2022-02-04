a0abf7f008c921a6fe9d03cecdd65231eb5d0393
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-02-04 14:21:16 -0500

    added console.log and return difference

--- lighthouse-js-fundamentals/hello.js
+++ lighthouse-js-fundamentals/hello.js
@@ -1,12 +1,31 @@
 //first 
-/*const sayHello = function(){
+/*
+const sayHello = function(){
   console.log("Hello, world");
 }
-sayHello();*/
+sayHello();
+*/
+
 // add parameter (name)
+/*
 const sayHello = function(name){
   console.log("Hello, " + name);
 }
 sayHello("Tanner");
 sayHello("Caliban");
 sayHello("Miranda");
+*/
+
+// console.log prints to console
+const sayHelloToConsole = function (name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+// return nothing prints to console
+const returnSayHello = function (name){
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+//added console.log to print to console
+console.log(greeting);
