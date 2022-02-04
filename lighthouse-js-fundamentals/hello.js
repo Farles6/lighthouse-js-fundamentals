f25b85d2a2c7fcbf83b7f17648fe5f5b69292222
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-02-04 13:47:40 -0500

    sayHello function

---
+++ lighthouse-js-fundamentals/hello.js
@@ -0,0 +1,12 @@
+//first 
+/*const sayHello = function(){
+  console.log("Hello, world");
+}
+sayHello();*/
+// add parameter (name)
+const sayHello = function(name){
+  console.log("Hello, " + name);
+}
+sayHello("Tanner");
+sayHello("Caliban");
+sayHello("Miranda");
